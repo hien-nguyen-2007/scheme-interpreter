2d8cf5f487e38cda13296d272cbd0ddc02337696
Hien Nguyen <82673040+RespectForOreos@users.noreply.github.com>
2024-12-02 09:53:56 -0800

    Completed problem 11

--- scheme_forms.py
+++ scheme_forms.py
@@ -44,7 +44,6 @@ def do_define_form(expressions, env):
         # defining a named procedure e.g. (define (f x y) (+ x y))
         # BEGIN PROBLEM 10
         "*** YOUR CODE HERE ***"
-    
         validate_formals(signature.rest)    
         env.define(signature.first, LambdaProcedure(signature.rest, expressions.rest, env))
         return signature.first
@@ -229,6 +228,7 @@ def do_mu_form(expressions, env):
     validate_formals(formals)
     # BEGIN PROBLEM 11
     "*** YOUR CODE HERE ***"
+    return MuProcedure(formals, expressions.rest)
     # END PROBLEM 11
 
 
